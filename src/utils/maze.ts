b0a8761e81c1e1ff3ab9574399cf4a4312b40780
Gabe Serna <gabriel.serna118@gmail.com>
2024-12-17 17:44:50 -0500

    Stop project

--- src/utils/maze.ts
+++ src/utils/maze.ts
@@ -1,7 +1,10 @@
 import labyrinthos from "labyrinthos";
 
 export default function createMaze() {
-  const mainMap = new labyrinthos.TileMap({ width: 64, height: 64 });
+  const mainMap = new labyrinthos.TileMap({
+    width: 64,
+    height: 64,
+  });
   mainMap.fill(1);
   mainMap.seed(1794841256);
   mainMap.defaultRogueLike = ["•", " "];
