209c1b63c7bf7571a25be2f430958f7e2aaa4b41
Gabe Serna <gabriel.serna118@gmail.com>
2024-12-17 11:09:09 -0500

    Render Map with New Mask

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,27 @@ import createMaze from "./scripts/maze";
 
 function App() {
   const pre = useRef<HTMLPreElement>(null!);
-  const maze = createMaze();
+  const map = createMaze();
 
   useEffect(() => {
-    pre.current.innerText = maze.toString();
-  }, [maze]);
+    pre.current.innerText = map.mask().toString();
+  }, [map]);
+
+  window.document.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (event.key === "ArrowUp") {
+      map.y += 1;
+      console.log(map.y);
+      pre.current.innerText = map.mask().toString();
+    }
+  });
 
   return (
-    <>
-      <pre ref={pre} className="mx-auto w-fit" />
-    </>
+    <section
+      id="game"
+      className="flex min-h-screen items-center justify-center"
+    >
+      <pre ref={pre} className="w-fit leading-[0.6]" />
+    </section>
   );
 }
 
--- src/scripts/maze.ts
+++ src/scripts/maze.ts
@@ -1,11 +1,12 @@
 import labyrinthos from "labyrinthos";
 
 export default function createMaze() {
-  const mainMap = new labyrinthos.TileMap({ width: 32, height: 32 });
+  const mainMap = new labyrinthos.TileMap({ width: 64, height: 64 });
   mainMap.fill(1);
+  mainMap.seed(1794841256);
+  mainMap.defaultRogueLike = ["•", " "];
 
-  console.log(labyrinthos.mazes);
-  labyrinthos.mazes.AldousBroder(mainMap, 0, 0);
+  labyrinthos.mazes.AldousBroder(mainMap);
 
-  return mainMap.mask();
+  return mainMap;
 }
